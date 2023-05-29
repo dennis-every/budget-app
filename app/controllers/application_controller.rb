25485d4a5fc41a34ce3083aabe53d85118278e7b
Dennis Every <dennis@ansyra.com>
2023-05-29 16:34:40 -0400

    Add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :devise_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def devise_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Categories', type: :request do
+  describe 'GET /index' do
+    context 'when user is not signed in' do
+      it 'redirects to the sign-in page' do
+        get categories_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'when user is signed in' do
+      let(:user) do
+        User.create(
+          name: 'John',
+          email: "#{Faker::Name.first_name}@test.com",
+          password: 'password',
+          password_confirmation: 'password'
+        )
+      end
+
+      before do
+        sign_in user
+      end
+
+      it 'renders the index template' do
+        get categories_path
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+end
